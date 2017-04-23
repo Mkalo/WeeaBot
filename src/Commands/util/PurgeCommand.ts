d68c5f628cc0db15d86f5249d141ba21949daea6
Mkalo <mkalo@xtibia.com>
2017-04-23 17:20:15 -0300

    Use better purge command.

--- src/Commands/util/PurgeCommand.ts
+++ src/Commands/util/PurgeCommand.ts
@@ -1,4 +1,6 @@
-import { Collection, Message } from 'discord.js';
+/* See also: https://github.com/WeebDev/Hamakaze */
+
+import { Collection, GuildMember, Message, User } from 'discord.js';
 import { Command, CommandMessage, CommandoClient } from 'discord.js-commando';
 import { sendSimpleEmbeddedError, sendSimpleEmbeddedSuccess } from '../../Library/Misc';
 
@@ -6,10 +8,16 @@ export default class PurgeCommand extends Command {
 	constructor(client: CommandoClient) {
 		super(client, {
 			name: 'purge',
-			aliases: ['del'],
+			aliases: ['clean', 'prune', 'clear'],
 			group: 'util',
 			memberName: 'purge',
-			description: 'Deletes messages. Permission to manage messages required.',
+			description: 'Deletes messages.',
+			details: `Deletes messages. Here is a list of filters:
+				__invites:__ Messages containing an invite
+				__user @user:__ Messages sent by @user
+				__bots:__ Messages sent by bots
+				__uploads:__ Messages containing an attachment
+				__links:__ Messages containing a link`,
 			guildOnly: true,
 			throttling: {
 				usages: 2,
@@ -18,28 +26,67 @@ export default class PurgeCommand extends Command {
 
 			args: [
 				{
-					key: 'n',
-					prompt: 'Amount of messages to delete, default = 1.',
+					key: 'limit',
+					prompt: 'how many messages would you like to delete?\n',
 					type: 'integer',
+					max: 100
+				},
+				{
+					key: 'filter',
+					prompt: 'what filter would you like to apply?\n',
+					type: 'string',
+					default: '',
+					parse: (str: string) => str.toLowerCase()
+				},
+				{
+					key: 'member',
+					prompt: 'whose messages would you like to delete?\n',
+					type: 'member',
 					default: ''
 				}
 			]
 		});
 	}
 
-	public async run(msg: CommandMessage, args: { n: string }): Promise<Message | Message[]> {
-		if (!msg.member.hasPermission('MANAGE_MESSAGES')) {
-			return sendSimpleEmbeddedError(msg, 'You don\'t have permission to use this command.', 5000);
+	public hasPermission(msg: CommandMessage): boolean {
+		return this.client.isOwner(msg.author) || msg.member.hasPermission('MANAGE_MESSAGES');
+	}
+
+	public async run(msg: CommandMessage, args: { limit: number, filter: string, member: GuildMember }): Promise<Message | Message[]> {
+		const { filter, limit } = args;
+		let messageFilter: (message: Message) => boolean;
+
+		if (filter) {
+			if (filter === 'invite') {
+				messageFilter = (message: Message) => message.content.search(/(discord\.gg\/.+|discordapp\.com\/invite\/.+)/i) !== -1;
+			} else if (filter === 'user') {
+				if (args.member) {
+					const { member } = args;
+					const user: User = member.user;
+					messageFilter = (message: Message) => message.author.id === user.id;
+				} else {
+					return msg.say(`${msg.author}, you have to mention someone.`);
+				}
+			} else if (filter === 'bots') {
+				messageFilter = (message: Message) => message.author.bot;
+			} else if (filter === 'you') {
+				messageFilter = (message: Message) => message.author.id === this.client.user.id;
+			} else if (filter === 'upload') {
+				messageFilter = (message: Message) => message.attachments.size !== 0;
+			} else if (filter === 'links') {
+				messageFilter = (message: Message) => message.content.search(/https?:\/\/[^ \/\.]+\.[^ \/\.]+/) !== -1;
+			} else {
+				return msg.say(`${msg.author}, that is not a valid filter. \`help clean\` for all available filters.`);
+			}
+
+			const messages: Collection<string, Message> = await msg.channel.fetchMessages({ limit });
+			const messagesToDelete: Collection<string, Message> = messages.filter(messageFilter);
+
+			msg.channel.bulkDelete(messagesToDelete.array().reverse()).catch((err: Error) => null);
+			return null;
 		}
 
-		let n: number = Number(args.n);
-		if (isNaN(n) || n <= 0) {
-			n = 1;
-		}
-		return msg.channel.bulkDelete(n + 1).then((value: Collection<string, Message>) =>
-			sendSimpleEmbeddedSuccess(msg, `Deleted ${n} message${n > 1 ? 's' : ''}`, 5000)
-		).catch(() =>
-			sendSimpleEmbeddedError(msg, 'An error ocurred while deleting the messages.', 5000)
-		);
+		msg.channel.bulkDelete(limit).catch((err: Error) => null);
+		return null;
 	}
 }
